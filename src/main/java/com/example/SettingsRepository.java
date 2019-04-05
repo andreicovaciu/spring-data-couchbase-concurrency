0c7f0f286f64a808ad5f804ab8a90a5eb4b53ea0
Andrei Covaciu <andrei_covaciu98@yahoo.com>
2019-04-05 16:47:23 +0300

    Documented the solution

--- src/main/java/com/example/SettingsRepository.java
+++ src/main/java/com/example/SettingsRepository.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.data.couchbase.core.query.N1qlPrimaryIndexed;
 import org.springframework.data.couchbase.core.query.Query;
 import org.springframework.data.couchbase.repository.CouchbasePagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 
 @N1qlPrimaryIndexed
@@ -14,6 +13,14 @@ public interface SettingsRepository extends CouchbasePagingAndSortingRepository<
     @Query("#{#n1ql.selectEntity} WHERE #{#n1ql.filter}")
     Iterable<Settings> findAll();
 
-    @Query("#{#n1ql.selectEntity} WHERE #{#n1ql.filter} AND property = '#{#property}'")
-    Settings findByProperty(@Param("property") Value property);
+    /**
+     * The solution is to explicitly write the query
+     *     <code>
+     *         @Query("#{#n1ql.selectEntity} WHERE #{#n1ql.filter} AND property = '#{#property}'")
+     *         Settings findByProperty(@Param("property") Value property);
+     *     </code>
+     * @param property
+     * @return
+     */
+    Settings findByProperty(Value property);
 }
